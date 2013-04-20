c4a5b1506f06b4d30fb53aac8c3c5574ad22e1d8
SelrahcD <c.desneuf@gmail.com>
2013-04-20 23:17:30 +0200

    Add functions

--- application/repositories/LibraryRepository.php
+++ application/repositories/LibraryRepository.php
@@ -1,9 +1,22 @@
 <?php
 
 class LibraryRepository {
+
+	public function make($data = array())
+	{
+		$library = new Library;
+		$library->user_id = Auth::user()->id;
+
+		return $library;
+	}
 	
 	public function getById($libraryId)
 	{
 		return Library::find($libraryId);
+	}
+
+	public function store(Library $library)
+	{
+		return $library->save();
 	}
 }

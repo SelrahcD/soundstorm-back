61ef8a7137ebac7349afddc2b6fe0f39cfda287f
SelrahcD <c.desneuf@gmail.com>
2013-04-21 00:07:33 +0200

    Indent

--- application/controllers/library.php
+++ application/controllers/library.php
@@ -14,9 +14,9 @@ class Library_Controller extends Base_Controller {
 	public function get_show($libraryId)
 	{
 		if(!($library = $this->libraryRepository->getById($libraryId)))
-			{
-				return Response::error('404');
-			}
+		{
+			return Response::error('404');
+		}
 
 		return Response::json($library->to_array());
 	}
@@ -24,6 +24,26 @@ class Library_Controller extends Base_Controller {
 	public function post_create()
 	{
 		$library = $this->libraryRepository->make(Input::json());
+
+		if(!$library->valid())
+		{
+			return Response::json(array(
+				'error' => $library->errors));
+		}
+
+		$this->libraryRepository->store($library);
+
+		return Response::json($library->to_array());
+	}
+
+	public function put_update($libraryId)
+	{
+		if(!($library = $this->libraryRepository->getById($libraryId)))
+		{
+			return Response::error('404');
+		}
+
+		$library->fill(Input::json());
 
 		if(!$library->valid())
 		{

e33ea3cb67bb13991cc842f5c9409ff286653db4
SelrahcD <c.desneuf@gmail.com>
2013-04-21 11:09:11 +0200

    Store soundcloud data

--- application/repositories/UserRepository.php
+++ application/repositories/UserRepository.php
@@ -12,11 +12,14 @@ class UserRepository {
 		return User::where('soundcloud_id', '=', $soundcloudId)->first();
 	}
 
-	public function createUser($soundcloudUser)
+	public function createUser($soundcloudUser, $accessToken)
 	{
 		$user = new User;
 		$user->username = $soundcloudUser->username;
 		$user->soundcloud_id = $soundcloudUser->id;
+		$user->soundcloud_token = $accessToken['access_token'];
+		$user->soundcloud_refresh = $accessToken['refresh_token'];
+		$user->soundcloud_expiration = time() + $accessToken['expires_in'];
 		$user->save();
 
 		return $user;

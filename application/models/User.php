2327bdea69617dc03aca61df30b540b9543ec7ea
SelrahcD <c.desneuf@gmail.com>
2013-04-21 11:37:30 +0200

    Renew soundcloud token

--- application/models/User.php
+++ application/models/User.php
@@ -1,5 +1,36 @@
 <?php
 
 class User extends Eloquent {
+
+	public function setTokenData($data = array())
+	{
+		$this->soundcloud_token      = $data['access_token'];
+		$this->soundcloud_refresh    = $data['refresh_token'];
+		$this->soundcloud_expiration = time() + $data['expires_in'];
+		$this->save();
+	}
+
+	public function getSoundcloudToken()
+	{
+		if($this->soundcloud_expiration < time())
+		{
+			$this->renewToken();
+		}
+
+		return $this->soundcloud_token;
+	}
+
+	protected function renewToken()
+	{
+		$soundcloudService = Ioc::resolve('soundcloudService');
+
+		$soundcloudService->setAccessToken($this->soundcloud_token);
+
+		$accessData = $soundcloudService->accessTokenRefresh($this->soundcloud_refresh);
+		
+		$this->setTokenData($accessData);
+
+		$this->save();
+	}
 	
 }

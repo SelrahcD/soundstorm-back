b04e3f2f9452100a05b31a6538a1a8561088a829
SelrahcD <c.desneuf@gmail.com>
2013-04-21 10:02:46 +0200

    Bind seouncloud service

--- application/bindings.php
+++ application/bindings.php
@@ -14,3 +14,11 @@ Ioc::singleton('trackRepository', function()
 {
 	return new TrackRepository;
 });
+
+Ioc::singleton('soundcloudService', function()
+{
+	return new Services_Soundcloud(
+			Config::get('soundcloud.client_id'),
+			Config::get('soundcloud.client_secret'),
+			URL::to_action('auth@callback'));
+});

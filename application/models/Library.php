172212627f0e0b56661ed7d86bff1fa9fbcc695e
SelrahcD <c.desneuf@gmail.com>
2013-04-21 08:59:04 +0200

    Link to tracks

--- application/models/Library.php
+++ application/models/Library.php
@@ -4,4 +4,14 @@ class Library extends Aware {
 	
 	public static $table = 'libraries';
 
+	public function tracks()
+	{
+		return $this->has_many('Track');
+	}
+
+	public function addTrack(Track $track)
+	{
+		$this->tracks()->insert($track);
+	}
+
 }
